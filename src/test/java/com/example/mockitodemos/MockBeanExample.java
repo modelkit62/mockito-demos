efbd1010c56d5e277589b50ca76f062807386b0f
d r <modelkit62@gmail.com>
2020-04-13 20:35:53 +0100

    xxx

---
+++ src/test/java/com/example/mockitodemos/MockBeanExample.java
@@ -0,0 +1,37 @@
+package com.example.mockitodemos;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MockBeanExample {
+
+    @MockBean
+    UserRepository mockRepository;
+
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    public void givenCountMethodMocked_WhenCountInvoked_ThenMockValueReturned() {
+        Mockito.when(mockRepository.count()).thenReturn(123L);
+
+        UserRepository userRepoFromContext = context.getBean(UserRepository.class);
+        long userCount = userRepoFromContext.count();
+
+        Assert.assertEquals(123L, userCount);
+        Mockito.verify(mockRepository).count();
+    }
+
+    private class UserRepository {
+        public long count() {
+            return 0;
+        }
+    }
+}
